eb93e2c596454613e334211a888c3fa84e9f622e
Maya <breno0810@gmail.com>
2025-11-09 17:05:56 -0300

    getters e setters

--- src/Model/Aluno.java
+++ src/Model/Aluno.java
@@ -5,4 +5,42 @@ public class Aluno extends Pessoa {
     private Nota nota;
     private Curso curso;
     private Integer semestre;
+
+    
+    public Long getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(Long matricula) {
+        this.matricula = matricula;
+    }
+    
+    
+    public Nota getNota() {
+        return nota;
+    }
+
+    public void setNota(Nota nota) {
+        this.nota = nota;
+    }
+
+    
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    
+    public Integer getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(Integer semestre) {
+        this.semestre = semestre;
+    }
+
+
 }
--- src/Model/Professor.java
+++ src/Model/Professor.java
@@ -7,4 +7,38 @@ public class Professor extends Pessoa {
     private Titulacao titulacao;
     private List<Disciplina> disciplinas;
     private List<Turma> turmas;
+
+    public Long getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(Long matricula) {
+        this.matricula = matricula;
+    }
+    
+    public Titulacao getTitulacao() {
+        return titulacao;
+    }
+
+    public void setTitulacao(Titulacao titulacao) {
+        this.titulacao = titulacao;
+    }
+    
+    public List<Disciplina> getDisciplinas() {
+        return disciplinas;
+    }
+
+    public void setDisciplinas(List<Disciplina> disciplinas) {
+        this.disciplinas = disciplinas;
+    }
+
+    public List<Turma> getTurmas() {
+        return turmas;
+    }
+
+    public void setTurmas(List<Turma> turmas) {
+        this.turmas = turmas;
+    }
+    
+
 }
