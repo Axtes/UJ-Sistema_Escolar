45d44b479fefac7b2a1468fbb3c72afa4b4cdbdf
Maya <breno0810@gmail.com>
2025-11-09 17:18:54 -0300

    Getters e Setters

--- src/Model/Disciplina.java
+++ src/Model/Disciplina.java
@@ -1,4 +1,31 @@
 package Model;
 
 public class Disciplina {
+    private String nome;
+    private Integer codDisciplina;
+    private StatusDeConclusao statusDeConclusao;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getcodDisciplina() {
+        return codDisciplina;
+    }
+
+    public void setcodDisciplina(Integer codDisciplina) {
+        this.codDisciplina = codDisciplina;
+    }
+
+    public StatusDeConclusao getStatusDeConclusao() {
+        return statusDeConclusao;
+    }
+
+    public void setStatusDeConclusao(StatusDeConclusao statusDeConclusao) {
+        this.statusDeConclusao = statusDeConclusao;
+    }
 }
