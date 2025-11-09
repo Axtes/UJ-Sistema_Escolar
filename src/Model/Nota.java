718efc5adc2bd9233564427b54e1a2b2f735e4c3
Paulo Mateus <paulomateuss.sousa@icloud.com>
2025-11-09 17:47:10 -0300

    alteracao em notas

--- src/Model/Nota.java
+++ src/Model/Nota.java
@@ -1,4 +1,40 @@
 package Model;
 
 public class Nota {
+    private double nota_AV1;
+    private double nota_AV2;
+    private double nota_AV3;
+    private double recuperacao;
+
+    public double getNota_AV1() {
+        return nota_AV1;
+    }
+
+    public void setNota_AV1(double nota_AV1) {
+        this.nota_AV1 = nota_AV1;
+    }
+
+    public double getNota_AV2() {
+        return nota_AV2;
+    }
+
+    public void setNota_AV2(double nota_AV2) {
+        this.nota_AV2 = nota_AV2;
+    }
+
+    public double getNota_AV3() {
+        return nota_AV3;
+    }
+
+    public void setNota_AV3(double nota_AV3) {
+        this.nota_AV3 = nota_AV3;
+    }
+
+    public double getRecuperacao() {
+        return recuperacao;
+    }
+
+    public void setRecuperacao(double recuperacao) {
+        this.recuperacao = recuperacao;
+    }
 }
