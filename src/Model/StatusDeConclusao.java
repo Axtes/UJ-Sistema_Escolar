51a49ea6838267c568f200a6b6823e7312954fd8
Axtes <david.ssantos684@gmail.com>
2025-11-09 16:43:13 -0300

    version 0.0.4

--- src/Model/StatusDeConclusao.java
+++ src/Model/StatusDeConclusao.java
@@ -1,4 +1,19 @@
 package Model;
 
 public enum StatusDeConclusao {
+    APROVADO("Aprovado(a)"),
+    REPROVADO("Reprovado(a)"),
+    EM_ANDAMENTO("Em andamento"),
+    TRANCADO("Trancado"),
+    DISPENSADO("Dispensado(a)");
+
+    private final String status;
+
+    StatusDeConclusao(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
 }
--- src/Model/Titulacao.java
+++ src/Model/Titulacao.java
@@ -1,4 +1,19 @@
 package Model;
 
 public enum Titulacao {
+    DOUTOR("Doutor(a)"),
+    MESTRE("Mestre(a)"),
+    ESPECIALISTA("Especialista"),
+    GRADUADO("Graduado(a)");
+
+
+    private final String titulo;
+
+    Titulacao(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
 }
